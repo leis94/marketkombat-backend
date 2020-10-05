753329e9119f5eefe07a185c82d343734c10dab0
Daniel <bukaro.dany@gmail.com>
2020-10-05 04:36:12 +0000

    Deleted DEBUG flag on settings/production.py

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,7 @@ from .base import env
 # Base
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['vsbuy.xyz'])
-DEBUG = True
+
 
 # Databases
 DATABASES['default'] = env.db('DATABASE_URL')  # NOQA
